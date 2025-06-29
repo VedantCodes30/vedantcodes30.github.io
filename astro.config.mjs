6f7cb6edf772817905d7a50ed105dcf870487d09
Vedant Kale <vedantcodes30@gmail.com>
2025-06-29 20:31:06 +0530

    'fix base in astro config'

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://vedantcodes30.github.io",
-  base: "vedant.github.io",
+  base: "vedantcodes30.github.io",
 });
