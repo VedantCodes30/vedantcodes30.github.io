1ba306556245d71e4f3ef2a5d8ddcd12b5e0268c
Vedant Kale <vedantcodes30@gmail.com>
2025-06-30 10:33:26 +0530

    'final fix might work this time'

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,4 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://vedantcodes30.github.io",
-  base: "vedantcodes30.github.io",
 });
